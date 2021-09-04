7a0277f011b82c6da9561dd5d62d794a5856ce15
Andrey <clorock575@gmail.com>
2021-09-04 23:16:30 +0300

    improve functionality

--- handlers/command_handlers.py
+++ handlers/command_handlers.py
@@ -1,3 +1,6 @@
+import datetime
+import aiogram.utils.markdown as md
+from aiogram.types import ParseMode
 from aiogram import types, Dispatcher
 from aiogram.dispatcher import FSMContext
 from states.states import UserRegistration, AddingTaskStates
@@ -45,11 +48,28 @@ async def cmd_show_today_tasks(message: types.Message):
     Отправляет список, добавленных за текущий день задач
     """
     db_session = message.bot.get('db')
-    get_tasks_list = select(Task).where(Task.user_id == message.from_user.id)
+    get_tasks_list = select(Task).where(Task.user_id == message.from_user.id, Task.date == datetime.date.today())
     async with db_session() as session:
         tasks_list = await session.execute(get_tasks_list)
-        for task in tasks_list.scalars():
-            print(f'Результат: {task.task_name, task.start_time, task.end_time}')
+        result_text = [
+            md.text(
+                md.text(f'Задача: {md.bold(task.task_name)} ID: {md.bold(task.id)}'),
+                md.text(f'Описание: {md.bold(task.description)}'),
+                md.text(f'Время начала: {md.bold(task.start_time)}'),
+                md.text(f'Время окончания: {md.bold(task.end_time)}'),
+                sep='\n'
+            ) for task in tasks_list.scalars()
+        ]
+
+        await message.bot.send_message(message.from_user.id, '\n\n'.join(result_text), parse_mode=ParseMode.MARKDOWN)
+
+
+async def delete_task(message: types.Message):
+    """
+    Позволяет удалить задачу по её id
+    """
+    await AddingTaskStates.delete_task.set()
+    await message.reply('Введите id задачи, которую хотиту удалить')
 
 
 def register_commands(dp: Dispatcher):
@@ -58,3 +78,4 @@ def register_commands(dp: Dispatcher):
     dp.register_message_handler(cmd_add_user, commands='adduser')
     dp.register_message_handler(cmd_add_task, commands='addtask')
     dp.register_message_handler(cmd_show_today_tasks, commands='todaytasks')
+    dp.register_message_handler(delete_task, commands='deletetask')
--- states/states.py
+++ states/states.py
@@ -2,12 +2,19 @@ from aiogram.dispatcher.filters.state import State, StatesGroup
 
 
 class UserRegistration(StatesGroup):
+    """
+    Состояния для процесса регистрации пользователя
+    """
     adding_user = State()
 
 
 class AddingTaskStates(StatesGroup):
+    """
+    Состояния для действий с задачами
+    """
     adding_task_name = State()
     adding_description = State()
     adding_start_time = State()
     adding_end_time = State()
     save_task = State()
+    delete_task = State()
